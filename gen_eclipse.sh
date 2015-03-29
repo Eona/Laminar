82233354b29a80995a6b186e0763403a6688b715
JimJarvis <jimfanspire@gmail.com>
2015-03-29 09:55:52 -0400

    Add special paths for mac OS X

--- gen_eclipse.sh
+++ gen_eclipse.sh
@@ -13,8 +13,15 @@ else
     echo $BUILD_DIR already exists.
 fi
 
+if [[ "$(whoami)" == "Eona" && "$OSTYPE" == "darwin"* ]]; then
+    echo Mac OS X detected.
+    EXTRA_FLAG="-DCUDA_TOOLKIT_ROOT_DIR=/Developer/NVIDIA/cuda-7.0 -DGTEST_ROOT=/opt/gtest/clang"
+else
+    EXTRA_FLAG=
+fi
+
 cd $BUILD_DIR
-cmake -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_CXX_COMPILER_ARG1=-std=c++11 -DCMAKE_BUILD_TYPE=Release ../src/
+cmake -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_CXX_COMPILER_ARG1=-std=c++11 -DCMAKE_BUILD_TYPE=Release ../src/ $EXTRA_FLAG 
 cd ..
 
 echo
