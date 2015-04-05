3523ce17e5408873d6247b676df8c4fc3900ada3
Jim Fan <jimfanspire@gmail.com>
2015-04-05 04:04:36 -0400

    Add a dummy.cu to discover linker errors (like multiple definition)

---
+++ src/dummy.cu
@@ -0,0 +1,18 @@
+/*
+ * Eona Studio (c) 2015
+ */
+
+#include "global_utils.h"
+#include "timer.h"
+#include "connection.h"
+#include "transfer_layer.h"
+#include "loss_layer.h"
+#include "parameter.h"
+#include "lstm_layer.h"
+#include "network.h"
+#include "gradient_check.h"
+
+int dummy()
+{
+	return 0;
+}
