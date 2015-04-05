3c2ce4894bfa6e6b4ffa88fb712f9caa89350a58
Jim Fan <jimfanspire@gmail.com>
2015-04-04 22:41:16 -0400

    Add ParameterContainer

--- src/parameter.h
+++ src/parameter.h
@@ -6,7 +6,29 @@
 #ifndef PARAMETER_H_
 #define PARAMETER_H_
 
+#include "global_utils.h"
 
+class ParameterContainer
+{
+public:
+	ParameterContainer(int numberOfParam = 1) :
+		paramValues(numberOfParam),
+		paramGradients(numberOfParam)
+	{ }
 
+	~ParameterContainer() =default;
+
+	vector<float> paramValues;
+	vector<float> paramGradients;
+
+	void reset()
+	{
+		for (int i = 0; i < paramValues.size(); ++i)
+		{
+			paramValues[i] = 0;
+			paramGradients[i] = 0;
+		}
+	}
+};
 
 #endif /* PARAMETER_H_ */
