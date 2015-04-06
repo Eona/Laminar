ae7a0785a466bee8a7e69ca619c69b1a1c807114
Jim Fan <jimfanspire@gmail.com>
2015-04-06 09:33:49 -0400

    Construct connection separately to ensure random engine order

--- src/test/recurrent_net_test.cu
+++ src/test/recurrent_net_test.cu
@@ -51,11 +51,47 @@ TEST(RecurrentNet, TemporalSkip)
 	auto l3 = Layer::make<CosineLayer>();
 	auto l4 = Layer::make<SquareLossLayer>();
 
+	// Naming: c<in><out>_<skip>
+	auto c12 = Connection::make<LinearConnection>(l1, l2);
+	auto c22 = Connection::make<LinearConnection>(l2, l2);
+	auto c22_1 = Connection::make<LinearConnection>(l2, l2);
+	auto c22_3 = Connection::make<LinearConnection>(l2, l2);
+	auto c23 = Connection::make<LinearConnection>(l2, l3);
+	auto c23_1 = Connection::make<LinearConnection>(l2, l3);
+	auto c23_2 = Connection::make<LinearConnection>(l2, l3);
+	auto c32 = Connection::make<LinearConnection>(l3, l2);
+	auto c32_3 = Connection::make<LinearConnection>(l3, l2);
+	auto c33 = Connection::make<LinearConnection>(l3, l3);
+	auto c33_1 = Connection::make<LinearConnection>(l3, l3);
+	auto c33_2 = Connection::make<LinearConnection>(l3, l3);
+	auto c34 = Connection::make<LinearConnection>(l3, l4);
+
+
 	RecurrentNetwork net;
 	net.set_input(input);
 	net.set_target(target);
 	net.set_max_temporal_skip(3);
 
+	net.add_layer(l1);
+
+	net.add_recurrent_connection(c22_1);
+	net.add_recurrent_connection(c22_3, 3);
+	net.add_recurrent_connection(c32_3, 3);
+	net.add_connection(c12);
+
+	net.add_layer(l2);
+
+	net.add_connection(c23);
+	net.add_recurrent_connection(c23_2, 2);
+	net.add_recurrent_connection(c23);
+	net.add_recurrent_connection(c33);
+	net.add_recurrent_connection(c33_2, 2);
+
+	net.add_layer(l3);
+	net.add_connection(c34);
+	net.add_layer(l4);
+
+/*
 	net.add_layer(l1);
 
 	net.new_recurrent_connection<LinearConnection>(l2, l2);
@@ -74,6 +110,7 @@ TEST(RecurrentNet, TemporalSkip)
 	net.add_layer(l3);
 	net.new_connection<LinearConnection>(l3, l4);
 	net.add_layer(l4);
+*/
 
 	gradient_check(net, 1e-2, 1);
 }
