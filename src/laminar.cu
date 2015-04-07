fac5cbdf2509e760a28571f6bb03fe71ae1b2f1d
Jim Fan <jimfanspire@gmail.com>
2015-04-07 00:01:29 -0400

    Starting LSTM debugging

--- src/laminar.cu
+++ src/laminar.cu
@@ -4,6 +4,8 @@
 #include "global_utils.h"
 #include "timer.h"
 #include "connection.h"
+#include "full_connection.h"
+#include "gated_connection.h"
 #include "transfer_layer.h"
 #include "loss_layer.h"
 #include "parameter.h"
@@ -11,73 +13,115 @@
 #include "network.h"
 #include "gradient_check.h"
 
+FakeRand& rand_conn = FakeRand::instance_connection();
+FakeRand& rand_prehis = FakeRand::instance_prehistory();
+FakeRand& rand_input = FakeRand::instance_input();
+FakeRand& rand_target = FakeRand::instance_target();
+
+#define make_full Connection::make<FullConnection>
+#define make_gated Connection::make<GatedConnection>
+
 int main(int argc, char **argv)
 {
-	FakeRand::instance_connection().set_rand_seq(vector<float> {
+	rand_conn.set_rand_seq(vector<float> {
 		0.163, 1.96, 1.09, 0.516, -0.585, 0.776, 1, -0.301, -0.167, 0.732
 	});
 
-	FakeRand::instance_connection().use_fake_seq();
-	FakeRand::instance_connection().use_uniform_rand(-1, 2);
-	FakeRand::instance_connection().set_rand_display(true);
+	rand_conn.use_fake_seq();
+	rand_conn.use_uniform_rand(0, 2);
+	rand_conn.set_rand_display(true);
 
-	FakeRand::instance_prehistory().set_rand_seq(vector<float> {
+	rand_prehis.set_rand_seq(vector<float> {
 		.3
 	});
 
-	vector<float> input { 1.2, -0.9, 0.57, -1.47, -3.08 }; //, 1.2, .31, -2.33, -0.89 };
-	vector<float> target { 1.39, 0.75, -0.45, -0.11, 1.55}; //, -.44, 2.39, 1.72, -3.06 };
+	vector<float> input {
+		1.2, -0.9, 0.57, -1.47, -3.08, 1.2, .31, -2.33, -0.89
+	};
+	vector<float> target {
+		1.39, 0.75, -0.45, -0.11, 1.55, -.44, 2.39, 1.72, -3.06
+	};
 
-	auto l1 = Layer::make<ScalorLayer>();
+	rand_input.use_uniform_rand(-2, 2); rand_target.use_uniform_rand(-2, 2);
+	rand_input.set_rand_display(true); rand_target.set_rand_display(true);
+	vec_apply(input, rand_input); cout << endl; vec_apply(target, rand_target);
+
+	auto inLayer = Layer::make<ConstantLayer>();
 
 	auto forgetGate = Layer::make<SigmoidLayer>();
+	auto inputGate = Layer::make<SigmoidLayer>();
+	auto cellHatLayer = Layer::make<TanhLayer>();
+	auto cellLayer = Layer::make<ConstantLayer>();
+	auto outputGate = Layer::make<SigmoidLayer>();
+	auto outLayer = Layer::make<ConstantLayer>();
 
+	auto lossLayer = Layer::make<SquareLossLayer>();
 
-	auto l2 = Layer::make<SigmoidLayer>();
-	auto l3 = Layer::make<TanhLayer>(); // gate
+	// Naming: c<in><out>_<skip>, or gated: g<in><gate><out>_<skip>
+	auto c_in_inputGate = make_full(inLayer, inputGate);
+	auto c_outLast_inputGate = make_full(outLayer, inputGate);
+	auto c_cellLast_inputGate = make_full(cellLayer, inputGate);
 
-	auto l4 = Layer::make<SquareLossLayer>();
+	auto c_in_forgetGate = make_full(inLayer, forgetGate);
+	auto c_outLast_forgetGate = make_full(outLayer, forgetGate);
+	auto c_cellLast_forgetGate = make_full(cellLayer, forgetGate);
 
-	// NOTE IMPORTANT RULE
-	// For recurrent gated connection conn[layer(alpha), layer(gate) => layer(beta)]
-	// where alpha is t-1 (or more) and gate/beta are the current t
-	// Must be added after you add gate & alpha, and before beta.
+	auto c_in_cellHat = make_full(inLayer, cellHatLayer);
+	auto c_outLast_cellHat = make_full(outLayer, cellHatLayer);
 
-	// Naming: c<in><out>_<skip>
-	// g<in><gate><out>_<skip>
-	auto c12 = Connection::make<FullConnection>(l1, l2);
-	auto c13 = Connection::make<FullConnection>(l1, l3);
+	auto g_cellHat_inputGate_cell = make_gated(cellHatLayer, inputGate, cellLayer);
+	auto g_cellLast_forgetGate_cell = make_gated(cellLayer, forgetGate, cellLayer);
 
-	auto g234_1 = Connection::make<GatedConnection>(l2, l3, l4);
-	auto g234_2 = Connection::make<GatedConnection>(l2, l3, l4);
-//
-//	auto c22_1 = Connection::make<LinearConnection>(l2, l2);
-//	auto c22_3 = Connection::make<LinearConnection>(l2, l2);
-//	auto c23_1 = Connection::make<LinearConnection>(l2, l3);
-//	auto c23_2 = Connection::make<LinearConnection>(l2, l3);
-//	auto c32_3 = Connection::make<LinearConnection>(l3, l2);
-//	auto c33_1 = Connection::make<LinearConnection>(l3, l3);
-//	auto c33_2 = Connection::make<LinearConnection>(l3, l3);
+	auto c_in_outputGate = make_full(inLayer, outputGate);
+	auto c_outLast_outputGate = make_full(outLayer, outputGate);
+	auto c_cell_outputGate = make_full(cellLayer, outputGate);
+
+	auto g_cell_outputGate_out = Connection::make<GatedTanhConnection>(cellLayer, outputGate, outLayer);
+
+	auto c_out_loss = Connection::make<ConstantConnection>(outLayer, lossLayer);
+
 
 	RecurrentNetwork net;
 	net.set_input(input);
 	net.set_target(target);
-//	net.set_max_temporal_skip(3);
+	net.set_max_temporal_skip(1);
 
-	net.add_layer(l1);
+	net.add_layer(inLayer);
 
-	net.add_connection(c13);
-	net.add_layer(l3);
+	net.add_connection(c_in_inputGate);
+	net.add_recurrent_connection(c_outLast_inputGate);
+	net.add_recurrent_connection(c_cellLast_inputGate);
 
-	net.add_connection(c12);
-	net.add_layer(l2);
+	net.add_layer(inputGate);
 
-	net.new_connection<FullConnection>(l2, l4);
-	net.new_connection<FullConnection>(l3, l4);
+	net.add_connection(c_in_forgetGate);
+	net.add_recurrent_connection(c_outLast_forgetGate);
+	net.add_recurrent_connection(c_cellLast_forgetGate);
 
-//	net.add_recurrent_connection(g234_1);
-//	net.add_recurrent_connection(g234_2, 2);
-	net.add_layer(l4);
+	net.add_layer(forgetGate);
+
+	net.add_connection(c_in_cellHat);
+	net.add_recurrent_connection(c_outLast_cellHat);
+	net.add_layer(cellHatLayer);
+
+	net.add_connection(g_cellHat_inputGate_cell);
+	net.add_recurrent_connection(g_cellLast_forgetGate_cell);
+
+	net.add_layer(cellLayer);
+
+	net.add_connection(c_in_outputGate);
+	net.add_recurrent_connection(c_outLast_outputGate);
+	net.add_connection(c_cell_outputGate);
+
+	net.add_layer(outputGate);
+
+	net.add_connection(g_cell_outputGate_out);
+
+	net.add_layer(outLayer);
+
+	net.add_connection(c_out_loss);
+
+	net.add_layer(lossLayer);
 
 	gradient_check(net, 1e-2, 1);
 /*
