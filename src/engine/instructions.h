b784836c622c092e5b91ea68d803c5b90b787177
Jim Fan <jimfanspire@gmail.com>
2015-04-10 22:15:21 -0400

    [NEW] Instruction and OpCode

---
+++ src/engine/instructions.h
@@ -0,0 +1,36 @@
+/*
+ * Eona Studio (c) 2015
+ */
+
+
+#ifndef INSTRUCTIONS_H_
+#define INSTRUCTIONS_H_
+
+#include "../global_utils.h"
+
+struct OpCode
+{
+	OpCode(string _name) :
+		name(_name)
+	{ }
+
+	string name;
+};
+
+struct Instruction
+{
+	Instruction(OpCode _code, vector<int>& _readAddrs, int _writeAddr) :
+		code(_code), readAddrs(_readAddrs), writeAddr(_writeAddr)
+	{ }
+
+	Instruction(OpCode _code, std::initializer_list<int> _readAddrs, int _writeAddr) :
+		code(_code), readAddrs(_readAddrs), writeAddr(_writeAddr)
+	{ }
+
+	OpCode code;
+	vector<int> readAddrs;
+	int writeAddr;
+};
+
+
+#endif /* INSTRUCTIONS_H_ */
