5468b46c7ad9a3c437dc0cd96869ee6e282ca34a
Danfei Xu <morphine1021@gmail.com>
2015-04-22 21:40:05 -0400

    .

--- src/backend/cublas/cuda_engine.h
+++ src/backend/cublas/cuda_engine.h
@@ -306,11 +306,11 @@ public:
 		cublasScopy(handle, reads[0]->LEN, reads[0]->device_data, 1, aux.device_data, 1);
 		op_func_dual_t h_func;
 		cudaMemcpyFromSymbol( &h_func, cu_square_loss_func, sizeof( op_func_t ) );
-		GPU_CHECKERROR(mat_op_kernel<<<aux.GRID_DIM, aux.BLOCK_DIM>>>( aux.device_data,
+		mat_op_kernel<<<aux.GRID_DIM, aux.BLOCK_DIM>>>( aux.device_data,
 														reads[0]->device_data,
 														reads[1]->device_data,
 														aux.LEN,
-														h_func ));
+														h_func );
 
 	    cublasSasum(handle, aux.LEN, aux.device_data, 1, write);
 	    GPU_CHECKERROR(cudaDeviceSynchronize());
