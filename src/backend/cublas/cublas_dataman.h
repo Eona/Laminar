960a263a0d31eb81dc64795a19f584d0f34a4c97
Danfei Xu <morphine1021@gmail.com>
2015-04-25 19:07:32 -0400

    data manager for cublas

---
+++ src/backend/cublas/cublas_dataman.h
@@ -0,0 +1,133 @@
+/*
+ * Eona Studio (c) 2015
+ */
+
+#ifndef BACKEND_VECMAT_VECMAT_FUNC_DATAMAN_H_
+#define BACKEND_VECMAT_VECMAT_FUNC_DATAMAN_H_
+
+#include "../../engine/data_manager.h"
+#include "../../utils/rand_utils.h"
+#include "cuda_engine.h"
+
+/**
+ * Functional data manager: the target tensor is an artificially constructed
+ * vector function applied to the input tensor
+ */
+class CublasDataManager :
+		public DataManager<CudaFloatMat>
+//		public GradientCheckable<>
+{
+public:
+	typedef CudaEngine::CudaFloatMatPtr DataPtr;
+
+	/**
+	 * @param randLow lower bound of random input uniform distr
+	 * @param randHigh higher bound of random input uniform distr
+	 */
+	vector<float> dummyData;
+
+	CublasDataManager(vector<float> dummyData) :
+		dummyData(dummyData)
+	{}
+
+
+	void load_input(DataPtr write, bool is_initialized, LearningPhase learnPhase)
+	{
+		if (!is_initialized)
+			write->reset(inputDim, batchSize);
+
+		int phase = to_int(learnPhase);
+		LMN_ASSERT_THROW(streamPos[phase] < streamSizes[phase],
+				DataException("load_input stream position out of bound"));
+
+//		*write = inputStreams[phase][streamPos[phase]];
+		write->to_device(&dummyData[0]);
+	}
+
+	void load_target(DataPtr write, bool is_initialized, LearningPhase learnPhase)
+	{
+		if (!is_initialized)
+			write->new_zeros(targetDim, batchSize);
+
+		int phase = to_int(learnPhase);
+		LMN_ASSERT_THROW(streamPos[phase] < streamSizes[phase],
+				DataException("load_target stream position out of bound"));
+
+		*write = targetStreams[phase][streamPos[phase]];
+	}
+
+	bool prepare_next_batch_impl(LearningPhase learnPhase)
+	{
+		int phase = to_int(learnPhase);
+		// proceed to the next item in stream
+		++ streamPos[phase];
+
+		// If point to last in stream, end of epoch = true
+		return streamPos[phase] == streamSizes[phase];
+	}
+
+	void reset_epoch_impl(LearningPhase phase)
+	{
+		this->streamPos[to_int(phase)] = 0;
+	}
+
+	Dimension input_dim() const
+	{
+		return { this->inputDim, this->batchSize };
+	}
+
+	Dimension target_dim() const
+	{
+		return { this->targetDim, this->batchSize };
+	}
+
+	int batch_size() const
+	{
+		return this->batchSize;
+	}
+
+protected:
+	// Helper: convert LearningPhase to int
+	static int to_int(LearningPhase phase)
+	{
+		return (int) enum2integral(phase);
+	}
+
+	/*********** Gradient checking ***********/
+/*	virtual void gradient_check_perturb_impl(
+				int changeItem, DimIndex dimIdx, float eps)
+	{
+		// assume input_rand is fully reset
+		// calculate the 1D index inside input_rand
+		input_rand[changeItem * inputDim * batchSize
+			+ this->indexer.linearize(dimIdx)] += eps;
+	}
+
+	virtual void gradient_check_restore_impl(
+			int lastChangeItem, DimIndex lastDimIdx, float lastEps)
+	{
+		input_rand[lastChangeItem * inputDim * batchSize
+			+ this->indexer.linearize(lastDimIdx)] -= lastEps;
+	}*/
+
+private:
+	int inputDim;
+	int targetDim;
+	int batchSize;
+
+	// Training/Validation/Testing sample sizes
+	std::array<int, LEARNING_PHASE_N> streamSizes;
+
+	// current position in input/target stream
+	std::array<int, LEARNING_PHASE_N> streamPos;
+
+	// accessed by enum2integral(Training/Validation/Testing)
+	std::array<vector<Vecmatf>, LEARNING_PHASE_N> inputStreams, targetStreams;
+
+	UniformRand<float> unirand;
+
+	// for restoring perturbed input
+	DimIndexEnumerator indexer;
+};
+
+#endif /* BACKEND_VECMAT_VECMAT_FUNC_DATAMAN_H_ */
