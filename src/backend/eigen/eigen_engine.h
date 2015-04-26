2c72bbe1dcdd7a6af58ac07f265c2aecec034ac9
Jim Fan <jimfanspire@gmail.com>
2015-04-26 18:53:06 -0400

    [NEW] EigenEngine, under dev

---
+++ src/backend/eigen/eigen_engine.h
@@ -0,0 +1,11 @@
+/*
+ * Eona Studio (c) 2015
+ */
+
+#ifndef BACKEND_EIGEN_EIGEN_ENGINE_H_
+#define BACKEND_EIGEN_EIGEN_ENGINE_H_
+
+#include <Eigen/Dense>
+
+
+#endif /* BACKEND_EIGEN_EIGEN_ENGINE_H_ */
